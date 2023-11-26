1a01de8bc026bb09c66d0084f6f109e3ceade5ff
Ben Potter <ben@coder.com>
2023-11-25 22:30:06 -0600

    sample app

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from flask import Flask, request, render_template_string
+import pandas as pd
+import io
 
-from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+HTML_TEMPLATE = '''
+<!doctype html>
+<html>
+<head><title>Data Processor</title></head>
+<body>
+  <h1>Data Processor</h1>
+  <form method="post">
+    <textarea name="data" rows="10" cols="50">{{ data }}</textarea><br>
+    <input type="submit" value="Process Data">
+  </form>
+  {% if result %}
+    <h2>Result:</h2>
+    <p>{{ result }}</p>
+  {% endif %}
+</body>
+</html>
+'''
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    initial_data = "A,B,C\n1,2,3\n4,5,6\n7,8,9"  # Pre-filled data
+
+    if request.method == 'POST':
+        data = request.form['data']
+        df = pd.read_csv(io.StringIO(data))
+        result = f"Mean of column 'B': {df['B'].mean()}"
+
+    return render_template_string(HTML_TEMPLATE, data=initial_data, result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
